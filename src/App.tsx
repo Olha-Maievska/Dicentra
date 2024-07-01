80ab44dee6b70ab362f8f8e39785ed134a7d1d2f
Olha Maievska <olhamaievska91@gmail.com>
2024-07-01 16:44:22 +0200

    delete basename

--- src/App.tsx
+++ src/App.tsx
@@ -19,85 +19,82 @@ import MyProfile from './pages/MyProfile'
 import Order from './pages/Order'
 import SelectedFlowers from './pages/Search'
 
-const router = createHashRouter(
-  [
-    {
-      path: '/',
-      element: <Layout />,
-      errorElement: <PageNotFound />,
-      children: [
-        {
-          element: <Home />,
-          index: true,
-        },
-        {
-          element: <OrderByPhoto />,
-          path: 'order_by_photo',
-        },
-        {
-          element: <Portal />,
-          path: 'portal',
-        },
-        {
-          element: <SinglePortal />,
-          path: 'portal/:portalId',
-        },
-        {
-          element: <ShippingAndPayment />,
-          path: 'shipping_and_payment',
-        },
-        {
-          element: <Shops />,
-          path: 'shops',
-        },
-        {
-          element: <Contacts />,
-          path: 'contacts',
-        },
-        {
-          element: <AboutUs />,
-          path: 'about_us',
-        },
-        {
-          element: <Promotions />,
-          path: 'promotions',
-        },
-        {
-          element: <Bouquets />,
-          path: 'bouquets',
-        },
-        {
-          element: <SingleBouquet />,
-          path: 'bouquets/:bouquetsId',
-        },
-        {
-          element: <Cart />,
-          path: 'cart',
-        },
-        {
-          element: <Login />,
-          path: '/login',
-        },
-        {
-          element: <Registration />,
-          path: '/registration',
-        },
-        {
-          element: <MyProfile />,
-          path: 'my_profile',
-        },
-        {
-          element: <Order />,
-          path: 'order',
-        },
-        {
-          element: <SelectedFlowers />,
-          path: 'selected_flowers',
-        },
-      ],
-    },
-  ],
-  { basename: '/dicentra' }
-)
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        element: <Home />,
+        index: true,
+      },
+      {
+        element: <OrderByPhoto />,
+        path: 'order_by_photo',
+      },
+      {
+        element: <Portal />,
+        path: 'portal',
+      },
+      {
+        element: <SinglePortal />,
+        path: 'portal/:portalId',
+      },
+      {
+        element: <ShippingAndPayment />,
+        path: 'shipping_and_payment',
+      },
+      {
+        element: <Shops />,
+        path: 'shops',
+      },
+      {
+        element: <Contacts />,
+        path: 'contacts',
+      },
+      {
+        element: <AboutUs />,
+        path: 'about_us',
+      },
+      {
+        element: <Promotions />,
+        path: 'promotions',
+      },
+      {
+        element: <Bouquets />,
+        path: 'bouquets',
+      },
+      {
+        element: <SingleBouquet />,
+        path: 'bouquets/:bouquetsId',
+      },
+      {
+        element: <Cart />,
+        path: 'cart',
+      },
+      {
+        element: <Login />,
+        path: '/login',
+      },
+      {
+        element: <Registration />,
+        path: '/registration',
+      },
+      {
+        element: <MyProfile />,
+        path: 'my_profile',
+      },
+      {
+        element: <Order />,
+        path: 'order',
+      },
+      {
+        element: <SelectedFlowers />,
+        path: 'selected_flowers',
+      },
+    ],
+  },
+])
 
 export const App = () => <RouterProvider router={router} />
