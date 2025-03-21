e6089ca3c88a0e6b1d8d5459e7c672792beeef89
Olha Maievska <olhamaievska91@gmail.com>
2025-03-21 22:52:34 +0100

    set sale date

--- src/pages/Promotions/data/index.ts
+++ src/pages/Promotions/data/index.ts
@@ -1,6 +1,6 @@
 export const promotionData = {
   imgPath: 'promotion.jpg',
-  dataUntil: '2024-12-24',
-  title: 'Winter sale up to 50%.',
+  dataUntil: '2024-04-30',
+  title: 'Spring sale up to 50%.',
   promocode: 'sale50',
 }
